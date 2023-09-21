30d402511fb289f98d56af641b0111f793c3472e
BDS <113209967+BDS-001@users.noreply.github.com>
2023-09-21 09:45:55 -0400

    submit button gets passwords

--- form_validation.js
+++ form_validation.js
@@ -1,1 +1,7 @@
 const submitButton = document.querySelector('#submit-form')
+submitButton.addEventListener('click', function(){
+    const password = document.querySelector('#password')
+    const confirmPassword  = document.querySelector('#confirmPassword')
+
+    console.log(password.value)
+})
